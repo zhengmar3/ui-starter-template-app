f546d48750cea270e990b1364f8fbb0036306539
Zheng Ma <zheng.ma@r3.com>
2023-06-13 09:54:14 +0100

    init devs setup.

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import { Button } from "@mui/material";
+
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      <Button variant="contained" color="secondary">
+        Success
+      </Button>
+    </>
+  );
+}
+
+export default App;
